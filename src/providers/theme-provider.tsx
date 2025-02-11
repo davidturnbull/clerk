1129f81a7cd70ca63e37b134de84172b0031a622
David Turnbull <david.t@canva.com>
2025-02-11 13:31:40 +1100

    set up ThemeProvider

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemeProvider>) {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}
