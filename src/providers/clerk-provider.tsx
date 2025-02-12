3540d0e7c6d01f98f0cd261604c9adeb75778f7c
David Turnbull <david.t@canva.com>
2025-02-12 17:15:01 +1100

    refactor

--- src/providers/clerk-provider.tsx
+++ src/providers/clerk-provider.tsx
@@ -3,20 +3,16 @@
 import { ClerkProvider as DefaultClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
+import * as React from "react";
 
-interface ClerkProviderProps {
-  children: React.ReactNode;
-}
-
-export function ClerkProvider({ children }: ClerkProviderProps) {
+export function ClerkProvider(
+  props: React.ComponentProps<typeof DefaultClerkProvider>
+) {
   const { resolvedTheme } = useTheme();
 
   const isDarkTheme = resolvedTheme === "dark";
   const baseTheme = isDarkTheme ? dark : undefined;
+  const appearance = { ...props.appearance, baseTheme };
 
-  return (
-    <DefaultClerkProvider appearance={{ baseTheme }}>
-      {children}
-    </DefaultClerkProvider>
-  );
+  return <DefaultClerkProvider {...props} appearance={appearance} />;
 }
